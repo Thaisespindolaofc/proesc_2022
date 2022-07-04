1536869dd0c4295ec97828d6c615302e30dcea2c
Thais Brito <thaisbrito0917@gmail.com>
2022-07-03 23:49:17 -0300

    resolucoes

--- desafio_tecnico_proesc_2022/desafio3/app/Http/Controllers/CategoryController.php
+++ desafio_tecnico_proesc_2022/desafio3/app/Http/Controllers/CategoryController.php
@@ -14,7 +14,8 @@ class CategoryController extends Controller
     }
     public function index()
     {
-     return view('categorias-cadastrar', ['categories' => $categories] );
+     $categories = \App\Models\Category::all();
+     return view('categorias', ['categories' => $categories] );
     }
 
     public function listar()
@@ -25,7 +26,8 @@ class CategoryController extends Controller
 
     public function create()
     {
-     return view('categorias.create', ['category' => '']);
+     $categories = \App\Models\Category::all();
+     return view('categorias.create', ['category' => $categories]);
     }
 
     public function store(Request $request)
